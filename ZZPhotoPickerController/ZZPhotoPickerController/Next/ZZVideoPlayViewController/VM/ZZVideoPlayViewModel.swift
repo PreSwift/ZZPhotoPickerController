8e159bc02c303b076d1cd995666835c2ce61cfb7
WES319 <WES319MAC@wes319demac-mini.local>
2018-08-08 18:31:09 +0800

    1

---
+++ ZZPhotoPickerController/ZZPhotoPickerController/Next/ZZVideoPlayViewController/VM/ZZVideoPlayViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ZZVideoPlayViewModel.swift
+//  ZZPhotoPickerController
+//
+//  Created by WES319 on 8/8/18.
+//  Copyright © 2018年 周元素. All rights reserved.
+//
+
+import UIKit
+
+class ZZVideoPlayViewModel: NSObject {
+
+}
